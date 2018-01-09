b59d67f87be29b86b9b2eb48784cd58282861046
Brendon Earl <bearl@uvic.ca>
2018-01-09 12:22:00 -0800

    Refactor App & write app styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { AppContainer, AppHeader, AppLogo, AppTitle } from "./AppSC";
+import Dashboard from "./Dashboard/Dashboard";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <AppContainer>
+    <AppHeader>
+      <AppLogo src={logo} alt="logo" />
+      <AppTitle>Welcome to React</AppTitle>
+    </AppHeader>
+    <Dashboard />
+  </AppContainer>
+);
 
 export default App;
---
+++ src/AppSC.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+// Styled Collection
+
+export const AppContainer = styled.div`
+  text-align: center;
+`;
+
+export const AppHeader = styled.header`
+  background-color: #222;
+  height: 150px;
+  padding: 20px;
+  color: white;
+`;
+
+export const AppLogo = styled.img`
+  animation: App-logo-spin infinite 20s linear;
+  height: 80px;
+`;
+
+export const AppTitle = styled.h1`
+  font-size: 1.5em;
+`;
