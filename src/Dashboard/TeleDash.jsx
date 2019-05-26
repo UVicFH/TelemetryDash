de3ceab70569f908888f581edf9c9611414fbdde
Jayden Chan <jaydencn7@gmail.com>
2019-05-26 09:55:30 -0700

    JC: More data mangling updates

--- src/Dashboard/TeleDash.jsx
+++ src/Dashboard/TeleDash.jsx
@@ -37,21 +37,21 @@ class TeleDash extends Component {
                 data={this.props.data.test[this.props.data.test.length - 1].val}
                 possibleRange={[0,20]}
                 size={[400,40]}
-                unit={"%"}                
-              /> 
+                unit={"%"}
+              />
               <SingleBarGraph
                 valueName={"Break"}
                 data={this.props.data.brake[this.props.data.brake.length - 1].val}
                 possibleRange={[0,100]}
                 size={[400,40]}
-                unit={"%"}                
+                unit={"%"}
               />
-              <SingleBarGraph 
+              <SingleBarGraph
                 valueName={"Throttle"}
-                data={this.props.data.throttle[this.props.data.throttle.length - 1].val} 
+                data={this.props.data.throttle[this.props.data.throttle.length - 1].val}
                 possibleRange={[0,100]}
-                size={[400,40]} 
-                unit={"%"}                
+                size={[400,40]}
+                unit={"%"}
               />
             </CardBody>
           </Card>
@@ -60,115 +60,115 @@ class TeleDash extends Component {
               <span className="cardLabel">Engine</span>
             </CardHeader>
             <CardBody display="flex">
-              <DialGraph 
+              <DialGraph
                 valueName={"Test"}
                 units={"%"}
-                data={this.props.data.test[this.props.data.test.length - 1].val} 
+                data={this.props.data.test[this.props.data.test.length - 1].val}
                 size={[100,80]}
                 acceptableRange={[0,100]}
                 possibleRange={[0,50]}
-              />              
-              <DialGraph 
+              />
+              <DialGraph
                 valueName={"Throttle Position"}
                 units={"%"}
-                data={this.props.data.TPS[this.props.data.TPS.length - 1].val} 
+                data={this.props.data.TPS[this.props.data.TPS.length - 1].val}
                 size={[100,80]}
                 acceptableRange={[0,100]}
                 possibleRange={[0,100]}
               />
-              <DialGraph 
-                valueName={"Spark Advance"} 
+              <DialGraph
+                valueName={"Spark Advance"}
                 units={"°"}
-                data={this.props.data.spkadv[this.props.data.spkadv.length - 1].val} 
-                size={[100,80]} 
+                data={this.props.data.spkadv[this.props.data.spkadv.length - 1].val}
+                size={[100,80]}
                 acceptableRange={[20,65]}
                 possibleRange={[0,65]}
               />
-              <DialGraph 
-                valueName={"Pulse Width"} 
+              <DialGraph
+                valueName={"Pulse Width"}
                 units={"ms"}
-                data={this.props.data.pw[this.props.data.pw.length - 1].val} 
-                size={[100,80]} 
+                data={this.props.data.pw[this.props.data.pw.length - 1].val}
+                size={[100,80]}
                 acceptableRange={[1,9]}
                 possibleRange={[0,12]}
               />
-              <DialGraph 
-                valueName={"Duty"} 
+              <DialGraph
+                valueName={"Duty"}
                 units={"%"}
-                data={this.props.data.duty[this.props.data.duty.length - 1].val} 
-                size={[100,80]} 
+                data={this.props.data.duty[this.props.data.duty.length - 1].val}
+                size={[100,80]}
                 acceptableRange={[0,100]}
                 possibleRange={[0,100]}
               />
-              <DialGraph 
-                valueName={"AFR"} 
+              <DialGraph
+                valueName={"AFR"}
                 units={""}
-                data={Math.round(this.props.data.AFR[this.props.data.AFR.length - 1].val * 10) / 10} 
-                size={[100,80]} 
+                data={this.props.data.AFR[this.props.data.AFR.length - 1].val}
+                size={[100,80]}
                 acceptableRange={[8,15]}
                 possibleRange={[0,22]}
               />
             </CardBody>
           </Card>
           <Card>
-            <CardHeader>    
-              <span className="cardLabel">Temperatures</span>        
+            <CardHeader>
+              <span className="cardLabel">Temperatures</span>
             </CardHeader>
             <CardBody>
               {/* coolant needs alert over 190 */}
-            <SingleBarGraphAverage 
-                valueName={"Test"} 
-                data={this.props.data.test[this.props.data.test.length - 1].val} 
-                size={[400,40]} 
+            <SingleBarGraphAverage
+                valueName={"Test"}
+                data={this.props.data.test[this.props.data.test.length - 1].val}
+                size={[400,40]}
                 acceptableRange={[165,190]}
                 possibleRange={[-20,20]}
                 units={"°"}
               />
-              <SingleBarGraphAverage 
-                valueName={"Coolant"} 
-                data={this.props.data.engineTemp[this.props.data.engineTemp.length - 1].val} 
-                size={[400,40]} 
+              <SingleBarGraphAverage
+                valueName={"Coolant"}
+                data={this.props.data.engineTemp[this.props.data.engineTemp.length - 1].val}
+                size={[400,40]}
                 acceptableRange={[165,190]}
                 possibleRange={[40,200]}
                 units={"°"}
               />
-              <SingleBarGraphAverage 
-                valueName={"Manifold Air"} 
-                data={this.props.data.MAT[this.props.data.MAT.length - 1].val} 
-                size={[400,40]} 
+              <SingleBarGraphAverage
+                valueName={"Manifold Air"}
+                data={this.props.data.MAT[this.props.data.MAT.length - 1].val}
+                size={[400,40]}
                 acceptableRange={[35,110]}
                 possibleRange={[35,125]}
                 units={"°"}
               />
-              <SingleBarGraphAverage 
-                valueName={"Controller"} 
-                data={this.props.data.controllerTemp[this.props.data.controllerTemp.length - 1].val} 
-                size={[400,40]}                 
+              <SingleBarGraphAverage
+                valueName={"Controller"}
+                data={this.props.data.controllerTemp[this.props.data.controllerTemp.length - 1].val}
+                size={[400,40]}
                 acceptableRange={[0,50]}
                 possibleRange={[-30,90]}
                 units={"°"}
               />
-              <SingleBarGraphAverage 
-                valueName={"FETMOS High"} 
-                data={this.props.data.FETMOSHigh[this.props.data.FETMOSHigh.length - 1].val} 
-                size={[400,40]} 
+              <SingleBarGraphAverage
+                valueName={"FETMOS High"}
+                data={this.props.data.FETMOSHigh[this.props.data.FETMOSHigh.length - 1].val}
+                size={[400,40]}
                 acceptableRange={[0,120]}
                 possibleRange={[0,120]}
                 units={"°"}
               />
-              <SingleBarGraphAverage 
-                valueName={"FETMOS Low"} 
-                data={this.props.data.FETMOSLow[this.props.data.FETMOSLow.length - 1].val} 
-                size={[400,40]} 
+              <SingleBarGraphAverage
+                valueName={"FETMOS Low"}
+                data={this.props.data.FETMOSLow[this.props.data.FETMOSLow.length - 1].val}
+                size={[400,40]}
                 acceptableRange={[0,120]}
                 possibleRange={[0,120]}
                 units={"°"}
               />
-              {/* accumulator needs alert over 100 */}              
-              <SingleBarGraphAverage 
-                valueName={"Accumulator"} 
-                data={this.props.data.accumulatorTemp[this.props.data.accumulatorTemp.length - 1].val} 
-                size={[400,40]} 
+              {/* accumulator needs alert over 100 */}
+              <SingleBarGraphAverage
+                valueName={"Accumulator"}
+                data={this.props.data.accumulatorTemp[this.props.data.accumulatorTemp.length - 1].val}
+                size={[400,40]}
                 acceptableRange={[0,100]}
                 possibleRange={[0,120]}
                 units={"°"}
@@ -179,40 +179,40 @@ class TeleDash extends Component {
         <DashboardColumn>
           <Card>
             <CardBody>
-              <RPM 
-                size={[400,200]} 
-                data={16} 
+              <RPM
+                size={[400,200]}
+                data={16}
                 dataRPM = {this.props.data.test[this.props.data.test.length - 1].val*50}
                 dataSpeed = {this.props.data.speed[this.props.data.speed.length - 1].val}
-                possibleRangeRPM={[0,12300]} 
-              />     
-              <SingleBarGraph 
-                valueName={"Gas"} 
+                possibleRangeRPM={[0,12300]}
+              />
+              <SingleBarGraph
+                valueName={"Gas"}
                 data={this.props.data.fuel[this.props.data.fuel.length - 1].val}
-                size={[400,40]} 
-                acceptableRange={[0,100]}                
-                possibleRange={[0,100]} 
+                size={[400,40]}
+                acceptableRange={[0,100]}
+                possibleRange={[0,100]}
                 unit={"%"}
               />
               <SingleBarGraph
                 valueName={"Charge"}
                 data={this.props.data.charge[this.props.data.charge.length - 1].val}
                 size={[400,40]}
-                acceptableRange={[0,100]}                
-                possibleRange={[0,100]} 
+                acceptableRange={[0,100]}
+                possibleRange={[0,100]}
                 unit={"%"}
               />
-              <GearVisualization 
+              <GearVisualization
                 data={this.props.data.gear[this.props.data.gear.length - 1].val}
                 size={[400,40]}
               />
               {/* todo: break into wheels, rpm */}
-              <CarVisualization 
-                valueName={""} 
+              <CarVisualization
+                valueName={""}
                 data={this.props.data.test[this.props.data.test.length - 1].val}
-                size={[400,500]} 
-                max={25} 
-                min={0} 
+                size={[400,500]}
+                max={25}
+                min={0}
                 percent={true}
               />
             </CardBody>
@@ -225,7 +225,7 @@ class TeleDash extends Component {
             </CardHeader>
             <CardBody>
               <TimelineGraph size={[400,150]} max={10} min={0} valueName={"Charge"}
-                data = {[ 
+                data = {[
                   { "time": 1, "value": "2.9" },
                   { "time": 2, "value": "2.7" },
                   { "time": 3, "value": "4.3" },
@@ -248,7 +248,7 @@ class TeleDash extends Component {
               </CardHeader>
               <CardBody>
                 <TimelineGraph size={[400,150]} max={10} min={0} valueName={"Charge"}
-                  data = {[ 
+                  data = {[
                     { "time": 1, "value": "2.9" },
                     { "time": 2, "value": "2.7" },
                     { "time": 3, "value": "4.3" },
@@ -271,7 +271,7 @@ class TeleDash extends Component {
               </CardHeader>
               <CardBody>
                 <TimelineGraph size={[400,150]} max={10} min={0} valueName={"Charge"}
-                  data = {[ 
+                  data = {[
                     { "time": 1, "value": "2.9" },
                     { "time": 2, "value": "2.7" },
                     { "time": 3, "value": "4.3" },
--- src/TransformData.js
+++ src/TransformData.js
@@ -1,19 +1,50 @@
 export const acceptData = (existingData = {}, newData = {}) => {
   const nextData = Object.assign({}, existingData);
+
   Object.entries(newData).forEach(([key, value]) => {
-    if (key == 'hybrid/engine/AFR') {
-      if (Array.isArray(nextData[key])) {
-        nextData['AFR'].push({val: value});
-      } else {
-        nextData['AFR'] = [{val: value}];
-      }
+    const transformInfo = transformationInfo[key]
+      ? transformationInfo[key]
+      : null;
+
+    const nextKey = transformInfo ? transformInfo.key : key;
+    const map = transformInfo ? transformInfo.map : null;
+
+    if (Array.isArray(nextData[nextKey])) {
+      nextData[nextKey].push({
+        val: map ? map(value) : value,
+      });
     } else {
-      if (Array.isArray(nextData[key])) {
-        nextData[key].push(value);
-      } else {
-        nextData[key] = [value];
-      }
+      nextData[nextKey] = [{
+        val: map ? map(value) : value,
+      }];
     }
   });
+
   return nextData;
+};
+
+const roundToNDecimals = (n) => {
+  return (val) => {
+    const d = Math.pow(10, n);
+    return Math.round(val * d) / d;
+  };
+};
+
+const transformationInfo = {
+  'hybrid/engine/AFR': {
+    key: 'AFR',
+    map: roundToNDecimals(1),
+  },
+  'hybrid/engine/TPS': {
+    key: 'TPS',
+    map: roundToNDecimals(0),
+  },
+  'hybrid/dash/charge': {
+    key: 'charge',
+    map: roundToNDecimals(0),
+  },
+  'hybrid/dash/fuel': {
+    key: 'fuel',
+    map: roundToNDecimals(0),
+  },
 };
